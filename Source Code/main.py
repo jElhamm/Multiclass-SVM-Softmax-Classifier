3fcbe571fedb5e4580634011957db6ca811204e2
jelhamm <elhamjafari200312@gmail.com>
2023-12-13 22:06:13 +0330

    Model Training and Prediction

--- Source Code/main.py
+++ Source Code/main.py
@@ -16,4 +16,23 @@ def main():
     X = np.random.randn(num_samples, dim)                                   # Generate random samples
     y = np.random.randint(0, num_classes, size=num_samples)                 # Generate random labels for the samples
     classifier = Classifier()                                               # Create an instance of the classifier
- 
+
+    #******************************************************************************************************************************************
+    # Model training
+    # Prompt the user to enter the loss type (svm/softmax)
+    loss_type = input("---> Enter loss type (svm/softmax): ")
+    # Train the classifier using the provided loss type
+    loss_history = classifier.train(X, y, learning_rate=1e-3, reg=1e-5, num_iters=1000, batch_size=200, verbose=True, loss_type=loss_type)
+    # Prediction
+    # Predict labels for the input samples
+    y_pred = classifier.predict(X)
+    #******************************************************************************************************************************************
+
+    print("\n-----------------------------------------------------------------")
+    print("*** Predicted labels:", y_pred)
+    print("*** Loss history:", loss_history)
+    print("-----------------------------------------------------------------\n")
+
+
+if __name__ == "__main__":
+    main()
